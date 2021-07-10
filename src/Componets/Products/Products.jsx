86eb96f2fc03b366637cecc0aa006b425548b9cf
KasunHasanga <kasunhasanga96@gmail.com>
2021-07-10 20:07:58 +0530

    add styles prodcuts

--- src/Componets/Products/Products.jsx
+++ src/Componets/Products/Products.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import {Grid} from "@material-ui/core";
 import Product from "./Product/Product";
 
+import useStyle from './styles'
 
 const products =[
     {id:1,name:"shocks",description:"Running Shooese",price :"5$"},
@@ -9,7 +10,11 @@ const products =[
 ]
 
 const Products = () => {
-  return <main>
+
+  const classes=useStyle();
+
+  return <main classname={classes.content}>
+    <div  className={classes.toolbar} />
     <Grid container justify="center" spacing={4}>
         {products.map((product)=>(
             <Grid item key={product.id} xs={12} sm={6} ms={4} lg={3}>
