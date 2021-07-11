85137d6e851b8d73a218c714244ae418f73fa481
KasunHasanga <kasunhasanga96@gmail.com>
2021-07-11 16:21:24 +0530

    add change product count and empty card function

--- src/App.js
+++ src/App.js
@@ -14,9 +14,23 @@ const App = () => {
   };
 
   const handleAddToCart = async (productId, quantity) => {
-    const item = await commerce.cart.add(productId, quantity);
-    setCart(item.cart);
+    const {cart} = await commerce.cart.add(productId, quantity);
+    setCart(cart);
   };
+
+  const handleUpdateCartQty = async (productId, quantity) => {
+    const {cart} = await commerce.cart.update(productId, quantity);
+    setCart(cart);
+  };
+
+  const handleRemoveFromCart =async (productId) =>{
+    const {cart} =await commerce.cart.remove(productId);
+    setCart(cart);
+  }
+  const handleEmptyCart =async () =>{
+    const {cart} =await commerce.cart.empty();
+    setCart(cart);
+  }
 
   const fetchCart = async () => {
     setCart(await commerce.cart.retrieve());
@@ -27,7 +41,6 @@ const App = () => {
     fetchCart();
   }, []);
 
-  console.log(cart);
 
   return (
     <Router>
@@ -38,7 +51,12 @@ const App = () => {
             <Products products={products} onAddtoCart={handleAddToCart} />
           </Route>
           <Route exact path="/cart">
-            <Cart cart={cart} />
+            <Cart 
+            cart={cart}
+            handleUpdateCartQty ={handleUpdateCartQty}
+            handleRemoveFromCart={handleRemoveFromCart}
+            handleEmptyCart={handleEmptyCart}
+            />
           </Route>
         </Switch>
       </div>
