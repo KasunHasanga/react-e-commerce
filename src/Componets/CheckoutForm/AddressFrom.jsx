3ed939c2586ee8db97a72cfb2dc87c86daf58dcd
KasunHasanga <kasunhasanga96@gmail.com>
2021-07-19 23:10:30 +0530

    downgrade react-form-hook and add address form

--- src/Componets/CheckoutForm/AddressFrom.jsx
+++ src/Componets/CheckoutForm/AddressFrom.jsx
@@ -1,12 +1,31 @@
 import React from 'react';
 import {InputLabel,Select,MenuItem,Button,Grid,Typography} from "@material-ui/core";
-import {useForm,FormProvider} from 'react-hook-form'
+import {useForm,FormProvider} from 'react-hook-form';
+import FormInput from './CustomeTextFeild';
 
 const AddressFrom = () => {
+    const methods=useForm();
     return (
-        <div>
-            AddressFrom
-        </div>
+        <>
+             <Typography variant="h6" gutterBottom>Shipping Address</Typography>
+             <FormProvider {...methods}>
+                 <form onSubmit=''>
+                     <Grid container spacing={3}>
+                         <FormInput required name='firstName' label="First Name"/>
+                         <FormInput required name='lastName' label="Last Name"/>
+                         <FormInput required name='address1' label="Address"/>
+                         <FormInput required name='email' label="Email"/>
+                         <FormInput required name='city' label="City"/>
+                         <FormInput required name='zip' label="ZIP / Postal code "/> 
+                         <Grid>
+                            <InputLabel>Shipping Countnty</InputLabel>
+
+                        </Grid>
+                     </Grid>
+                 </form>
+
+             </FormProvider>
+        </>
     )
 }
 
